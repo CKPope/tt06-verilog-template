814956d12b46d623737e17f26b981fc97360520b
CKPope <166442118+CKPope@users.noreply.github.com>
2024-04-15 11:25:35 -0400

    Update test.py - extended testing time

--- test/test.py
+++ test/test.py
@@ -21,18 +21,18 @@ async def test_project(dut):
   dut.rst_n.value = 0
 
   dut._log.info("run for 10 clock cycles and then release reset")
-  await ClockCycles(dut.clk, 10)
+  await ClockCycles(dut.clk, 20)
   dut.rst_n.value = 1
 
   # Set the both target inputs to 0xF values, wait one clock cycle, wait one clock cycle, then activate MOTION_Input and wait 1 clock cycle, then deactivate Motion Input, then wait 20 clock cycles, then check the output
   dut._log.info("Test")
   dut.ui_in.value = 255
-  await ClockCycles(dut.clk, 1)
+  await ClockCycles(dut.clk, 40)
   
   dut.uio_in.value = 1
-  await ClockCycles(dut.clk, 1)
+  await ClockCycles(dut.clk, 40)
   
   dut.uio_in.value = 0
-  await ClockCycles(dut.clk, 20)
+  await ClockCycles(dut.clk, 1000)
   
   assert dut.uo_out.value == 255
