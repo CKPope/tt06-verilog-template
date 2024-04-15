c93539faf09c864f1e4ed1ac31a8b16fcb5d7b73
CKPope <166442118+CKPope@users.noreply.github.com>
2024-04-15 11:44:37 -0400

    Update test.py - added axis independent decrementing test

--- test/test.py
+++ test/test.py
@@ -24,15 +24,29 @@ async def test_project(dut):
   await ClockCycles(dut.clk, 20)
   dut.rst_n.value = 1
 
-  # Set the both target inputs to 0xF values, wait one clock cycle, wait one clock cycle, then activate MOTION_Input and wait 1 clock cycle, then deactivate Motion Input, then wait 20 clock cycles, then check the output
-  dut._log.info("Test")
+  # INCREMENTING TEST: Set the both target inputs to 0xF values for each axis, wait one clock cycle, then activate MOTION_Input and wait 2 clock cycles, then deactivate Motion Input, then run 20 clock cycles, then check the output for test1
+
+  dut._log.info("Test1")
   dut.ui_in.value = 255
-  await ClockCycles(dut.clk, 40)
+  await ClockCycles(dut.clk, 1)
   
   dut.uio_in.value = 1
-  await ClockCycles(dut.clk, 40)
+  await ClockCycles(dut.clk, 2)
   
   dut.uio_in.value = 0
-  await ClockCycles(dut.clk, 1000)
+  await ClockCycles(dut.clk, 20)
   
   assert dut.uo_out.value == 255
+
+  # INDEPENDENT DECREMENTING TEST:Set the both target inputs to 7 values (0 for one axis, 7 for the other axis), wait one clock cycle, then activate MOTION_Input and wait 2 clock cycles, then deactivate Motion Input, then run 20 clock cycles, then check the output for test2
+  dut._log.info("Test2")
+  dut.ui_in.value = 7
+  await ClockCycles(dut.clk, 1)
+  
+  dut.uio_in.value = 1
+  await ClockCycles(dut.clk, 2)
+  
+  dut.uio_in.value = 0
+  await ClockCycles(dut.clk, 20)
+  
+  assert dut.uo_out.value == 7
